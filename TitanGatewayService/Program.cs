46642532c31c0cb3d1e7a25c28ca0f3af171770f
Fike Rehman <fike.rehman@hotmail.com>
2025-11-11 19:37:06 -0600

    Adding structured logging with Serilog

--- TitanGatewayService/Program.cs
+++ TitanGatewayService/Program.cs
@@ -1,22 +1,31 @@
 using TitanGatewayService;
 using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
 
 var builder = Host.CreateApplicationBuilder(args);
 
-// 1. Configure Serilog
+var logDirectory = builder.Configuration["Logging:LogDirectory"] ?? "Logs";
+Directory.CreateDirectory(logDirectory);
+
 Log.Logger = new LoggerConfiguration()
     .Enrich.FromLogContext()
-    .WriteTo.Console() // This extension method is provided by Serilog.Sinks.Console
-    .WriteTo.File("logs/gateway-.log", rollingInterval: RollingInterval.Day)
+    .WriteTo.Console(
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+        theme: SystemConsoleTheme.Colored)
+    .WriteTo.File(
+        Path.Combine(logDirectory, "gateway-.log"),
+        rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
-// 2. Replace default logger
+// Replace default logger and add Serilog
 builder.Logging.ClearProviders();        // remove default console logger
 builder.Logging.AddSerilog(Log.Logger);  // use Serilog instead
 
-
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddSingleton<IDeviceConfigurationManager, DeviceConfigurationManager>();
+
+Log.Information("Starting Titan Gateway Service. Please Stand by ....");
 
 var host = builder.Build();
 host.Run();
